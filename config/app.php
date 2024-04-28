c98e8244776a1d56add5c9240c41526cdf8e8f12
Cletuh26 <ismaelx26@gmail.com>
2024-04-28 17:00:01 +0200

    fix(general): Solucionado error web

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Undead Secrets'),
+    'name' => env('APP_NAME', 'Undead-Secrets'),
 
     /*
     |--------------------------------------------------------------------------
