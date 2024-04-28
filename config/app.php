134de9681ebc0a34546fa99f84767052a12366bc
Cletuh26 <ismaelx26@gmail.com>
2024-04-28 16:40:22 +0200

    fix(readme): Correccion de errores

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'undead Secrets'),
+    'name' => env('APP_NAME', 'Undead Secrets'),
 
     /*
     |--------------------------------------------------------------------------
